04025d63ddc2708c4c0b4fee7f9d1ec3b7af786e
vivek <vivekkumarpatel0312@gmail.com>
2024-03-29 22:51:54 +0530

    backend portion completed

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -2,9 +2,10 @@ import express from 'express'
 
 const router = express.Router();
 
-import  {sendMessage} from '../controllers/message.controllers.js'
+import  {sendMessage, getMessages} from '../controllers/message.controllers.js'
 import protectRoute from '../middleware/protectRoute.js';
 
+router.get('/:id', protectRoute, getMessages);
 router.post('/send/:id', protectRoute, sendMessage);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,18 @@
 
 import express from 'express'
 import dotenv from 'dotenv'
-dotenv.config()
 import cookieParser from 'cookie-parser'
 
 import authRoutes from './routes/auth.routes.js' // with import we will have to use .js also
 import messageRoutes from './routes/message.routes.js'
+import userRoutes from './routes/user.routes.js'
+
+
 import connectToMongoDB from './database/db.js';
 
-
 const app = express();
+
+dotenv.config()
 
 app.use(express.json()) // to parse the incoming request with JSON payloads (from req.body)
 app.use(cookieParser());
@@ -20,6 +23,7 @@ app.use(cookieParser());
 // Routes - middleware
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
 
 // app.get('/', (req, res) => {
 //     res.send('Hello world!')
